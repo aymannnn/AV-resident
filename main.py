39cfa84e7338f3f39ab63e6b7220812e8a770944
aymannnn <ayman.ali1230@gmail.com>
2025-05-18 09:45:26 -0400

    fixed it so unfilled is rank 0 and done after others are sorted

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pandas as pd
 import numpy as np
 
 df = pd.read_csv('input_data/preferences_cleaned.csv', index_col=0)
+missing_preferences = pd.read_csv('input_data/people_missing.csv', header=None)
+people_to_ignore = missing_preferences[0].tolist()
 
 def get_preferences(df):
     preferences = {}
@@ -11,11 +13,17 @@ def get_preferences(df):
         preferences[person] = ordered_dates
     return preferences
 
-def get_random_order(people):
-    return np.random.permutation(people)
+def get_random_order(people, people_to_ignore):
+    people = list(people)
+    add_to_end = list(people_to_ignore)
+    # Remove people_to_ignore from people
+    people = [p for p in people if p not in add_to_end]
+    order = np.random.permutation(people)
+    order = list(order)
+    return order + add_to_end
 
 def assign_dates(df, preferences):
-    people = get_random_order(df.index)
+    people = get_random_order(df.index, people_to_ignore)
     sum = 0
     final_df = pd.DataFrame(index=df.index, columns=['Assignment', 'Rank'])
     available_dates = df.columns
@@ -35,7 +43,7 @@ def assign_dates(df, preferences):
                 break
     return final_df, sum
 
-def run_simulation(df, n_simulations=1000):
+def run_simulation(df, n_simulations=10000):
     best_assignments = None
     best_score = float('inf')
     preferences = get_preferences(df)
@@ -48,4 +56,4 @@ def run_simulation(df, n_simulations=1000):
     best_assignments.to_csv('output_data/assignments.csv', index=True)
     return True
 
-run_simulation(df, n_simulations=50000)
+run_simulation(df, n_simulations=10000)
